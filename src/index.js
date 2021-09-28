168fc1a537a787ef5b7bcfe5bc6a6be72f192c8e
Johan Forero <johanfore67@gmail.com>
2021-09-27 20:57:03 -0500

    Firebase Funcionando sin error al importa reactfire

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './app';
-// import {FirebaseAppProvider} from 'reactfire'
-// import firebaseConfig from './firebase-config'
+import {FirebaseAppProvider} from 'reactfire'
+import firebaseConfig from './firebase-config'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+    <App />
+  </FirebaseAppProvider>, document.getElementById('root'));
