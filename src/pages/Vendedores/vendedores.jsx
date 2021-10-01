de93b9c1c351308a76e2d7d8b62e6d11c8f64c03
isbolith <dec1984@msn.com>
2021-10-01 18:32:12 -0500

    Crear formulario de registro de productos

--- src/pages/Vendedores/vendedores.jsx
+++ src/pages/Vendedores/vendedores.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
-import LawFirm from '../../assets/img/law-firm.png';
-import Logo from '../../assets/img/logo.png';
-import Google from '../../assets/img/Google.png'
-import Title from './components/Title/title';
-import Label from './components/Label/label';
-import Input from './components/Input/input';
+import './components/formulario.css';
 
 export default function vendedores() {
 
   return (
     <div>
-      Pagina vendedores
+      <div className="contenedor">
+        <form className="formulario">
+          <center><h1>Registro de Productos</h1></center>
+          <br></br>
+          <div>
+            <label htmlFor="codigo">Código</label>
+            <input type="text" id="codigo" name="codigo" placeholder="123" />
+            <label htmlFor="nombre">Nombre del producto</label>
+            <input type="text" id="nombre" name="nombre" placeholder="producto marca x" />
+            <div className="row">
+              <div className="col-6">
+                <label htmlFor="valor">Valor unitario</label>
+                <input type="text" id="valor" name="valor" placeholder="$" />
+              </div>
+              <div className="col-6">
+                <label htmlFor="estado">Estado</label>
+                <input type="email" id="estado" name="estado" />
+              </div>
+            </div>
+            <div className="row">
+              <button type="submit">Ingresar producto</button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
