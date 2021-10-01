58be55e2f4614bc259a996341c4c5f94b3ef995b
JuanMaDLTS <jdela@unal.edu.co>
2021-10-01 18:20:21 -0500

    Interfaz de listar Usuarios

--- src/app.jsx
+++ src/app.jsx
@@ -4,13 +4,15 @@ import Ventas from './pages/Ventas/ventas';
 import Login from './pages/Login/login';
 import Home from './pages/Home/home';
 import Vendedores from './pages/Vendedores/vendedores';
-import listarProductos from './pages/Productos/productos'
+import listarProductos from './pages/Productos/productos';
+import Usuarios from './pages/Usuarios/usuarios';
 
 
 export function App(){
   return (
     <BrowserRouter>
     <Route exact path="/lproductos" component={listarProductos} />
+    <Route exact path="/usuarios" component = {Usuarios} />
     <Route exact path="/ventas" component={Ventas} />
     <Route exact path="/" component={Login} />
     <Route exact path="/home" component={Home} />
