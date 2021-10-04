b3630cd176d649bc06e43a233e106c0969081302
Johan Forero <johanfore67@gmail.com>
2021-10-03 22:18:32 -0500

    Arreglo interfaz - Login

--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -21,9 +21,9 @@ export default function Login() {
               <img src={Logo} alt="" />
               <h2>InnovAPPS</h2>
               <Title text="Log In" />
-              <p>Enter your email and password below</p>
+            {/* <p>Enter your email and password bel.contenedor .contentBx .formLoginow</p> */}
             </div>
-            <div className="form">
+            {/* <div className="form">
               <Label text="EMAIL" />
               <input className="input" type="email" placeholder="Enter your Email"  />
               <br />
@@ -31,8 +31,8 @@ export default function Login() {
               <input className="input" type="password" placeholder="Enter your Password"/>
               <button>Log In</button>
               <p>Or</p>
-            </div>
-          <a href="/home"><img src={Google} alt="" /></a>
+            </div> */}
+          <a href="/home" className="logo-google"><img src={Google} alt="" /></a>
           </div>
         </div>
     </section>
